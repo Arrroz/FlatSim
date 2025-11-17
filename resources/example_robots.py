9dea9c79926f6bd7a0cd74c6829c140c4666f182
Arrroz <jprmoreira@gmail.com>
2025-11-17 11:27:52 +0000

    added triped

--- resources/example_robots.py
+++ resources/example_robots.py
@@ -140,3 +140,48 @@ def wheeled_biped(camera: camera.Camera, ground_height=-1.5):
     model.set_state(0, body_y, 0, [np.pi/4, -np.pi/2, 0, -np.pi/4, np.pi/2, 0])
     
     return (model, controller)
+
+def triped(camera: camera.Camera, ground_height=-1.5):
+    body_length = 2.5
+    body_height = 0.8
+    leg_link_length = 0.8
+    body_mass = 15
+    leg_mass = 2
+
+    body = example_links.robot_body(camera=camera, width=body_length, height=body_height, mass=body_mass)
+    left_thigh = example_links.leg_link(camera=camera, length=leg_link_length, mass=leg_mass)
+    left_shin = example_links.leg_link(camera=camera, length=leg_link_length, mass=leg_mass)
+    middle_thigh = example_links.leg_link(camera=camera, length=leg_link_length, mass=leg_mass)
+    middle_shin = example_links.leg_link(camera=camera, length=leg_link_length, mass=leg_mass)
+    right_thigh = example_links.leg_link(camera=camera, length=leg_link_length, mass=leg_mass)
+    right_shin = example_links.leg_link(camera=camera, length=leg_link_length, mass=leg_mass)
+
+    left_hip = joint.RJoint(body, np.array([-body_length/2+0.2, -body_height/4]),
+                           left_thigh, np.array([-leg_link_length/2, 0]),
+                           offset=-np.pi/2)
+    left_knee = joint.RJoint(left_thigh, np.array([leg_link_length/2, 0]),
+                            left_shin, np.array([-leg_link_length/2, 0]))
+    middle_hip = joint.RJoint(body, np.array([0, -body_height/4]),
+                           middle_thigh, np.array([-leg_link_length/2, 0]),
+                           offset=-np.pi/2)
+    middle_knee = joint.RJoint(middle_thigh, np.array([leg_link_length/2, 0]),
+                            middle_shin, np.array([-leg_link_length/2, 0]))
+    right_hip = joint.RJoint(body, np.array([body_length/2-0.2, -body_height/4]),
+                            right_thigh, np.array([-leg_link_length/2, 0]),
+                            offset=-np.pi/2)
+    right_knee = joint.RJoint(right_thigh, np.array([leg_link_length/2, 0]),
+                             right_shin, np.array([-leg_link_length/2, 0]))
+    
+    foot_radius = 0.1 # TODO: foot_radius is hard coded from leg_link standard width
+
+    model = robot.Robot(body, [left_hip, left_knee, middle_hip, middle_knee, right_hip, right_knee])
+    lleg_controller = control.LegController([left_knee, left_hip], foot_radius=foot_radius, foot_anchor=np.array([leg_link_length/2, 0]))
+    mleg_controller = control.LegController([middle_knee, middle_hip], foot_radius=foot_radius, foot_anchor=np.array([leg_link_length/2, 0]))
+    rleg_controller = control.LegController([right_knee, right_hip], foot_radius=foot_radius, foot_anchor=np.array([leg_link_length/2, 0]))
+    controller = control.BodyController(body, [lleg_controller, mleg_controller, rleg_controller])
+    
+    body_y = ground_height + foot_radius + 0.8*np.sqrt(2) - left_hip.anchor_parent[1] + 1e-5
+    model.set_state(0, body_y, 0, [np.pi/4, -np.pi/2, np.pi/4, -np.pi/2, -np.pi/4, np.pi/2])
+    
+    return (model, controller)
+
